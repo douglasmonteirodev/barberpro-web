7bbb88c7947dc54123f54af3fbd808542a8ee402
Douglas Monteiro <douglasmonteiro071@gmail.com>
2023-01-03 21:06:10 -0300

    permanecer login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { destroyCookie, setCookie } from "nookies";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import { destroyCookie, setCookie, parseCookies } from "nookies";
 import Router from "next/router";
 
 import { api } from "../services/apiClient";
@@ -55,6 +55,29 @@ export function signOut() {
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
+
+  useEffect(() => {
+    const { "@barber.token": token } = parseCookies();
+
+    if (token) {
+      api
+        .get("/me")
+        .then((res) => {
+          const { id, name, endereco, email, subscriptions } = res.data;
+
+          setUser({
+            id,
+            name,
+            email,
+            endereco,
+            subscriptions,
+          });
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInProps) {
     try {
